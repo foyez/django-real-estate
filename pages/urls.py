65e1475ed7124d9e256cd3dc0033090952f3d4ff
foyez <foyezar@yahoo.com>
2019-08-01 21:59:48 +0600

    add static files, navbar & footer

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from pages import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(self):
-    return HttpResponse('<h1>Hello World</h1>')
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
